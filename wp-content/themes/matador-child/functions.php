b2755f9b8a2bc58b453fa9ae510eb315053cd701
EstebanCampos <esteban.campos@usach.cl>
2015-07-08 12:40:50 -0400

    Agregado Poll y Form

--- wp-content/themes/matador-child/functions.php
+++ wp-content/themes/matador-child/functions.php
@@ -5,3 +5,27 @@ This file does not replace the parent theme's function file.
 It is loaded in addition to the parent theme's functions file.
 ------------------------------------------------------------*/
 
+
+
+function matador_child_widgets_init(){
+
+  register_sidebar( array(
+    'name'          => __('Form sidebar', 'ThemeStockyard'),
+    'description'   => __('Sidebar con formulario.', 'ThemeStockyard'),
+    'id'            => 'form-sidebar',
+    'before_widget' => '<div id="%1$s" class="widget %2$s clearfix">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<div class="page-title clearfix"><h5 class="mimic-small uppercase subtle-text-color"><span>',
+    'after_title'   => '</span></h5></div>'
+  ));
+}
+
+add_action('widgets_init', 'matador_child_widgets_init');
+
+
+function matador_child_register_menu(){
+  register_nav_menu('new-menu', __('Nuevo Menu'));
+
+}
+
+add_action('init', 'matador_child_register_menu');
