737ffbc93e5db9dd1fe639220d3391eaa0e5421c
EstebanCampos <esteban.campos@usach.cl>
2015-07-16 22:23:00 -0400

    Basta con el includes en theme padre

--- wp-content/themes/matador-child/includes/theme-plugins.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*-----------------------------------------------------------------------------------
-
-- Loads all the .php files found in /includes/plugins/ directory
-
------------------------------------------------------------------------------------ */
-require_once(TS_SERVER_PATH . '/includes/plugins/tgm-plugin-activation/activate-plugins.php');
