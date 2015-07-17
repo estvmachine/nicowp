39fe951a669c0bb570f572e3cf0a2ecc859ee8ef
EstebanCampos <esteban.campos@usach.cl>
2015-07-16 21:59:05 -0400

    Agregado vista de list

--- wp-content/plugins/preguntas-semana/page_preguntas_semana.php
+++ wp-content/plugins/preguntas-semana/page_preguntas_semana.php
@@ -2,14 +2,15 @@
 //http://codex.wordpress.org/Class_Reference/wpdb
 //http://techslides.com/creating-a-wordpress-plugin-admin-page
 //http://techslides.com/a-z-category-index-wordpress-plugin
+
 global $wpdb;
 
 echo "<h2>Preguntas Semana Enpelootas</h2>";
 
-$arr = $wpdb->get_results("SELECT * FROM $wpdb->users");
+$arr = $wpdb->get_results("SELECT * FROM `wp_pregunta_semana`");
 
 echo '<div id="dt_example"><div id="container"><form><div id="demo">';
-echo '<table cellpadding="0" cellspacing="0" border="0" class="display" id="example"><thead><tr>';
+echo '<table class="wp-list-table widefat fixed striped posts"  id="example"><thead><tr>';
 
 foreach ($arr[0] as $k => $v) {
     echo "<td>".$k."</td>";
